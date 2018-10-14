694eb8345eac5ee4a1a788fc4f5ae1f69d1f177a
KateringCFH <kcristalfh@gmail.com>
2018-10-14 14:35:55 -0400

    added migration for drinks, recipes, measure, conversion and menu_drink

---
+++ taller2018/database/migrations/2018_10_14_172341_drink.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class A24Drink extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drink', function (Blueprint $table) {
+            $table->increments('id_drink')->unique();
+            $table->string('name',50);
+            $table->string('type',50)->unique();
+            $table->timestamp('caducity_date');
+            $table->timestamp('packaging_date');
+            $table->integer('id_administrador');
+            $table->integer('id_menu_drink');
+            $table->integer('id_meassure');
+            $table->integer('transaction_id');
+            $table->timestamp('transaction_date');
+            $table->string('transaction_host',50);
+            $table->string('transaction_user',50);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

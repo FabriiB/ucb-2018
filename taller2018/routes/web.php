b97a8161e18ce0075ceaf81369a3b62f7c86f452
Benji <benjimon313@gmail.com>
2018-11-06 13:28:14 -0400

    ¨web.php¨

--- taller2018/routes/web.php
+++ taller2018/routes/web.php
@@ -21,7 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 Route::get('/home', 'HomeController@index')->name('home');
-=======
 Route::resource('passports','PassportController');
 
 

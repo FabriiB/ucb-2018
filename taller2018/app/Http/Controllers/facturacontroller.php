a9320d3970288c6b0e162a8d676d6feabf0a9d50
Benji <benjimon313@gmail.com>
2018-11-07 19:24:18 -0400

    controller con bd

--- taller2018/app/Http/Controllers/facturacontroller.php
+++ taller2018/app/Http/Controllers/facturacontroller.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\facturarequest;
 use App\facturamodel;
+use Illuminate\Support\Facades\DB;
+use App\User;
+
 
 class facturacontroller extends Controller
 {
     public function index(Request $request)
     {
-        return view('Facturas.index');
+        $id=6140974;
+        $name=DB::table('users')
+            ->select('firs_name','last_name1', 'last_name2','id')
+            ->where('id','=',$id)
+            ->first();
+        return view('Facturas.index',["datos"=>$name]);
     }
 }

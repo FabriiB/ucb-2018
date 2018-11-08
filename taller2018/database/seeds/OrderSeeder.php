c30bd66700923f9159818ba03642c14a7af8911b
rodritm <rodritm2010@gmail.com>
2018-11-08 05:40:18 -0400

    Creacion de seeder para order

---
+++ taller2018/database/seeds/OrderSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($cont=2;$cont<=31;$cont++){
+            DB::table('order')->insert([
+                'idOrder' => "$cont",
+                'orderDate' => "'2018-11-07 23:27:57'",
+                'status' => 'En proceso',
+                'cancelDate' => "'2018-11-08 23:27:57'",
+                'idUser' => "$cont",
+                'idPlan' => "$cont",
+                'remember_token' => "'".$cont."'",
+                'created_at' => "'2018-11-07 23:27:57'",
+                'updated_at' => "'2018-11-07 23:27:58'"
+            ]);
+        }
+
+    }
+//INSERT INTO "public"."order" ("idOrder", "orderDate", "status", "cancelDate", "idUser", "idPlan", "remember_token", "created_at", "updated_at")
+//VALUES (1, '2018-11-07 23:27:57', 'En progreso', '2018-11-09 04:50:22', 1, 1, '1', '2018-11-07 23:50:44', '2018-11-07 23:59:49')
+}

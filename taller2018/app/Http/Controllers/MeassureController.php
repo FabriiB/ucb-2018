265d6e8f19c614da00903e061c06f32f604e113c
KateringCFH <kcristalfh@gmail.com>
2018-11-10 17:12:55 -0400

    meassure_v_2

--- taller2018/app/Http/Controllers/MeassureController.php
+++ taller2018/app/Http/Controllers/MeassureController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\MeassureRequest;
+use App\Meassure;
 
 class MeassureController extends Controller
 {
@@ -12,6 +15,40 @@ class MeassureController extends Controller
     }
     public function index(Request $request)
     {
-        return view('meassure.index');
+        if ($request) {
+            $query    = trim($request->get('searchText'));
+            $meassure = DB::table('meassure')
+                ->where('id_meassure', 'LIKE', '%' . $query . '%')
+                ->orwhere('name', 'LIKE', '%' . $query . '%')
+                ->orwhere('type', 'LIKE', '%' . $query . '%')
+                ->orderBy('id_meassure', 'asc')
+                ->paginate(5);
+            //return view('Recipe.index',compact('recipe'), ["searchText" => $query]);
+            return view('meassure.index', ["meassure" => $meassure, "searchText" => $query]);
+        }
+    }
+    public function create()
+    {
+        //echo "<script>alert('sale del create');</script>";
+        return view('meassure.create');
+    }
+    //Registra los datos ingresados
+    public function store(RecipeRequest $request)
+    {
+        //echo "<script>alert('entro al store');</script>";
+        $tid = '27';
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $tfecha = Carbon::now();
+        $meassure                    = new Recipe;
+        $meassure->unit       = $request->get('unit');
+        $meassure->name       = $request->get('name');
+        $meassure->type           = $request->get('type');
+        $meassure->transaction_id    = $tid;
+        $meassure->transaction_date  = $tfecha->format('Y-m-d H:i:s');
+        $meassure->transaction_host  = $ip;
+        $meassure->transaction_user  = $request->get('administrator');
+        $meassure->save();
+        //echo "<script>alert('salio del store');</script>";
+        return redirect()->action('MeassureController@index');
     }
 }

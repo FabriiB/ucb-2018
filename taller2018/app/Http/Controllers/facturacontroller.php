cf0cb82193acd3aefc020a93ad993bb769e20166
rodritm <rodritm2010@gmail.com>
2018-11-10 16:09:25 -0400

    modificaciones jueves 8/nov/2018

--- taller2018/app/Http/Controllers/facturacontroller.php
+++ taller2018/app/Http/Controllers/facturacontroller.php
@@ -14,7 +14,7 @@ class facturacontroller extends Controller
 {
     public function index(Request $request)
     {
-        $id=6140974;
+        $id=1;
         $name=DB::table('users')
             ->join('bill', 'users.id','=','bill.idUser')
             ->select('users.firs_name as firs_name','users.last_name1 as last_name1', 'users.last_name2 as last_name2','users.id as id', 'bill.control_code as control_code', 'bill.total_bill as total_bill', 'bill.description_bill as description_bill', 'bill.authorization_number as authorization_number','bill.identifier as identifier')

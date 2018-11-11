63b870314684b8b538d85754248aa905de521174
Benji <benjimon313@gmail.com>
2018-11-11 18:35:30 -0400

    Factura controller con nueva bd

--- taller2018/app/Http/Controllers/facturacontroller.php
+++ taller2018/app/Http/Controllers/facturacontroller.php
@@ -15,9 +15,11 @@ class facturacontroller extends Controller
     public function index(Request $request)
     {
         $id=1;
-        $name=DB::table('users')
-            ->join('bill', 'users.id','=','bill.idUser')
-            ->select('users.firs_name as firs_name','users.last_name1 as last_name1', 'users.last_name2 as last_name2','users.id as id', 'bill.control_code as control_code', 'bill.total_bill as total_bill', 'bill.description_bill as description_bill', 'bill.authorization_number as authorization_number','bill.identifier as identifier')
+        $name=DB::table('person')
+            ->join('bill', 'idPayment','=','bill.id_payment')
+            ->join('payment', 'id_user','=','payment.idUser')
+            ->select('person.firs_name as firs_name','person.last_name1 as last_name1', 'person.last_name2 as last_name2','person.id_person as id_person',
+                'bill.control_code as control_code', 'bill.total_bill as total_bill', 'bill.description_bill as description_bill', 'bill.authorization_number as authorization_number','bill.identifier as identifier')
             ->where('users.id','=',$id)
             ->orWhere('bill.idUser','=',$id)
             ->first();

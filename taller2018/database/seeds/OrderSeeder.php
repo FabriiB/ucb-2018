5e96917e73dda2a30b29116ecab78d98ab03ea92
zltm <jdavidrevillad@gmail.com>
2018-11-11 11:26:46 -0400

    DistributorsSeeder added, DatabaseSeeder modified to include distributors seeding

--- taller2018/database/seeds/OrderSeeder.php
+++ taller2018/database/seeds/OrderSeeder.php
@@ -12,19 +12,29 @@ class OrderSeeder extends Seeder
      */
     public function run()
     {
-        for ($cont=1;$cont<=31;$cont++){
+        DB::statement('ALTER TABLE "order" DISABLE TRIGGER ALL;');
+
+       for ($cont=40;$cont<=50;$cont++){
             DB::table('order')->insert([
                 'idOrder' => "$cont",
                 'orderDate' => "'2018-11-07 23:27:57'",
                 'status' => 'En proceso',
                 'cancelDate' => "'2018-11-08 23:27:57'",
-                'idUser' => "1",
+                //'idUser' => "1",
                 'idPlan' => "$cont",
                 'remember_token' => "'".$cont."'",
                 'created_at' => "'2018-11-07 23:27:57'",
-                'updated_at' => "'2018-11-07 23:27:58'"
+                'updated_at' => "'2018-11-07 23:27:58'",
+                'transaction_id' => "$cont",
+                'transaction_date' => "'2018-11-07 23:28:01'",
+                'transaction_host' => "'host tx'",
+                'transaction_user' => "'usuario tx'",
+                'id_person' => "$cont",
+                'id_menu_dish' => "$cont"
             ]);
         }
+
+        DB::statement('ALTER TABLE "order" ENABLE TRIGGER ALL;');
 
     }
 //INSERT INTO "public"."order" ("idOrder", "orderDate", "status", "cancelDate", "idUser", "idPlan", "remember_token", "created_at", "updated_at")

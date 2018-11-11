8ed9ec3f0addca03513a986f1678c2faebcfa830
Benji <benjimon313@gmail.com>
2018-11-11 19:44:14 -0400

    factura controller

--- taller2018/app/Http/Controllers/facturacontroller.php
+++ taller2018/app/Http/Controllers/facturacontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\facturarequest;
 use Illuminate\Support\Facades\DB;
-use App\User;
+use App\Bill;
 use Carbon\Carbon;
 
 
@@ -14,13 +14,12 @@ class facturacontroller extends Controller
     public function index(Request $request)
     {
         $id=1;
-        $name=DB::table('person')
-            ->join('payment', 'id_user','=','payment.idUser')
-            ->join('bill', 'idPayment','=','bill.id_payment')
-            ->select('person.firs_name as firs_name','person.last_name1 as last_name1', 'person.last_name2 as last_name2','person.id_person as id_person',
+        $name=DB::table('bill')
+            ->join('payment', 'bill.id_payment','=','payment.idPayment')
+            ->join('person', 'person.id_user','=','payment.idUser')
+            ->select('person.firs_name as firs_name','person.last_name1 as last_name1', 'person.last_name2 as last_name2','person.nit as nit',
                 'bill.control_code as control_code', 'bill.total_bill as total_bill', 'bill.description_bill as description_bill', 'bill.authorization_number as authorization_number','bill.identifier as identifier')
-            ->where('bill.id_payment','=',$id)
-            ->orWhere('payment.idUser','=',$id)
+            ->where('bill.id_bill','=',$id)
             ->first();
         $now = Carbon::now();
         return view('Facturas.index',["datos"=>$name, "now"=>$now]);

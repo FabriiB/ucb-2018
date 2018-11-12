66392d3c6127a2df3678b3ef127bc03889095181
KateringCFH <kcristalfh@gmail.com>
2018-11-12 10:37:50 -0400

    modelo de la tabla dish_ingedients

---
+++ taller2018/app/DishIngredients.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DishIngredients extends Model
+{
+    protected $table      = 'dish_ingredients';
+    protected $primaryKey = 'id_dish_ingredients';
+    public $timestamps    = false;
+    protected $fillable = [
+
+        'quantity',
+        'date_created',
+        'id_ingredients',
+        'id_didh',
+        'transaction_id',
+        'transaction_date',
+        'transaction_host',
+        'transaction_user',
+
+    ];
+    protected $guarded = [
+    ];
+}

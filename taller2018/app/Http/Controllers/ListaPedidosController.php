aa3d39ee9426c7c1d72686f58eeab919c45db502
rodritm <rodritm2010@gmail.com>
2018-11-15 03:28:50 -0400

    Modificacion filtro por fecha

--- taller2018/app/Http/Controllers/ListaPedidosController.php
+++ taller2018/app/Http/Controllers/ListaPedidosController.php
@@ -30,7 +30,7 @@ class ListaPedidosController extends Controller
                 ->where('order.orderDate','>', '20060719')//$fechainicial
                 ->where('order.orderDate','<', '20200529')//$fechafinal
                 ->orderBy('idOrder')
-                ->paginate(10);
+                ->paginate(5);
             return view('ListadoPedidos.index',["pedidos" => $pedidos],compact('pedidos'));
         }
 
@@ -105,6 +105,23 @@ class ListaPedidosController extends Controller
         //
     }
     public function filtro(Request $request){
-        $fechaini = $request->input('fechaiini');
+        $fechainicial = null;
+        $fechafinal = null;
+        if ($request) {
+
+            /*
+            $pedidos = DB::table('order')->select('idOrder', 'orderDate','status','id_person')->orderBy('idOrder')->paginate(10);
+            return view('ListadoPedidos.index',["pedidos" => $pedidos],compact('pedidos'));
+            */
+            $pedidos = DB::table('order')
+                ->select('order.idOrder', 'order.orderDate','order.status','person.firs_name','person.last_name1','person.last_name2')
+                ->join('person', 'person.id_person', '=', 'order.id_person')
+                ->where('order.orderDate','>', '20060719')//$fechainicial
+                ->where('order.orderDate','<', '20200529')//$fechafinal
+                ->orderBy('idOrder')
+                ->paginate(5);
+            return view('ListadoPedidos.index',["pedidos" => $pedidos],compact('pedidos'));
+        }
+
     }
 }

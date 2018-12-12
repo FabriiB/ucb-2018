e56a1eaf039a8dfd9781bce0051c1e002d2dd5ae
Garp <denfabrii@gmail.com>
2018-12-12 01:01:00 -0400

    borrando codigo inecesario

--- taller2018/resources/views/home/home.blade.php
+++ taller2018/resources/views/home/home.blade.php
@@ -26,19 +26,7 @@
                     </div>
                     @if($ordenes->count() > 0 )
                     <div class="row">
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
-
-                        <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-sm">
-                                <div class="modal-content">
-                                    ...
-                                </div>
-                            </div>
-                        </div>
                     @foreach ($ordenes as $orden)
-
-
-
                             <div class="col-md-4">
                             <div class="card card-profile">
                                 <div class="card-header card-header-image">
